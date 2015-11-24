5a33313907ee084d5b0901ab4a99b28a14a88349
agamestra <shyshygon@ya.ru>
2015-11-24 11:49:27 +0300

    pass

--- matrix.py
+++ matrix.py
@@ -12,13 +12,45 @@ class Matrix:
                 Mat[a]=[0]*n
 
     def __add__(self, other):
-
         pass
+
+    def determinant (self):
+        pass
+
+    def __eq__(self, other):
+        flag=0
+        for a in range (m):
+            for b in range (n):
+                if Mat[a,b]:
+                    pass
 
     def get(self, i, j):
         return Mat(i,j)
 
+    def get_m(self):
+        return self.m
+
+    def get_n(self):
+        return self.n
+
+    def get_size(self):
+        pass
+
+    def invert (self):
+        pass
+
+    def __mul__(self, other):
+        pass
+
     def set(self, i, j, value):
         Mat[i,j]=value
 
+    def __sub__(self, other):
+        pass
 
+    def transpose (self):
+        pass
+
+    def __truediv__(self, other):
+        pass
+    
